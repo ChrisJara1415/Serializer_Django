391d052abe12ce0ccad3347edb89d7e241598813
ChrisJara1415 <chris.jara0626@gmail.com>
2025-09-13 13:34:50 -0500

    Se agregan las rutas para conseguir un producto o una categoría por su respectivo ID

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('categorias/', views.lista_categorias, name='lista-categorias'),
     path('productos/', views.lista_productos, name='lista-productos'),
     path('marcas/', views.lista_marcas, name='lista-marcas'),
+    path('categorias/<int:pk>/', views.detalle_categoria, name='detalle_categoria'),
+    path('productos/<int:pk>', views.detalle_producto, name='detalle_producto'),
 ]
