3ae54aeb84a396942585bd9e45fc74598649588d
ChrisJara1415 <chris.jara0626@gmail.com>
2025-09-13 15:01:50 -0500

    Se agregan CRUD

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,10 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Listar
     path('categorias/', views.lista_categorias, name='lista-categorias'),
     path('productos/', views.lista_productos, name='lista-productos'),
     path('marcas/', views.lista_marcas, name='lista-marcas'),
+
+    # Detalles
     path("categorias/<int:pk>/", views.detalle_categoria, name='detalle_categoria'),
     path('productos/<int:pk>/', views.detalle_producto, name='detalle_producto'),
-    path('marcas/<int:pk>/', views.detalle_marca, name='detalle_marca')
+    path('marcas/<int:pk>/', views.detalle_marca, name='detalle_marca'),
+
+    # CRUD Categorías
+    path('categorias/crear/', views.crear_categoria, name='crear_categoría'),
+    path('categorias/<int:pk>/actualizar', views.actualizar_categoria, name='actualizar_categoría'),
+    path('categorias/<int:pk>/eliminar', views.eliminar_categoria, name='eliminar_categoría'),
+
+    # CRUD Productos
+    path('productos/crear', views.crear_producto, name='crear_producto'),
+    path('productos/<int:pk>/actualizar', views.actualizar_producto, name='actualizar_producto'),
+    path('productos/<int:pk>/eliminar', views.eliminar_producto, name='eliminar_producto'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -49,3 +49,55 @@ def detalle_marca(request, pk):
     marca = get_object_or_404(Marca, pk=pk)
     serializer = MarcaSerializer(marca)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def crear_categoria(request):
+    #Creamos un serializer con los datos enviados en la petición
+    serializer = CategoriaSerializer(data=request.data)
+    #Validamos si los datos son correctos
+    if serializer.is_valid():
+        serializer.save() #Guardamos en la Base de Datos
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #Si hay errores devolvemos el error en JSON
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["PUT"])
+def actualizar_categoria(request, pk):
+    #Buscamos la categoría para actualizar
+    categoria = get_object_or_404(Categoria, pk=pk)
+    #Pasamos el objeto y los datos nuevos al serializer
+    serializer = CategoriaSerializer(categoria, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["DELETE"])
+def eliminar_categoria (request, pk):
+    #Buscamos la categoría
+    categoria = get_object_or_404(Categoria, pk=pk)
+    categoria.delete() #Eliminamos
+    return Response({"mensaje": "Categoría eliminada"}, status=status.HTTP_204_NO_CONTENT)
+
+@api_view(["POST"])
+def crear_producto(request, pk):
+    serializer = ProductoSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(),
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["PUT"])
+def actualizar_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    serializer = ProductoSerializer(producto, data=request.data)
+    if serializer.is_valid():
+        serializer.save(),
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["DELETE"])
+def eliminar_producto(request, pk): 
+    categoria = get_object_or_404(Categoria, pk=pk)
+    categoria.delete()
+    return Response({"mensaje": "Categoría eliminada"}, status=status.HTTP_204_NO_CONTENT)
